105af504fdac7298a8b693dca05dd49e4ee21bd5
Duane Stafford <duanestafford@Duanes-MBP.richmond.edu>
2019-07-25 18:09:05 -0400

    spotify data

--- liri.js
+++ liri.js
@@ -3,63 +3,76 @@ require("dotenv").config();
 var axios = require('axios');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
- 
+
 var spotify = new Spotify(keys.spotify);
 
 var commands = process.argv[2];
 var value = process.argv[3];
+main();
 
-switch (commands) {
-    case "concert-this":
-        concertThis();
-        break;
-
-    case "spotify-this-song":
-        spotifySong();
-        break;
-
-    case "movie-this":
-
-        break;
-
-    case "do-what-it-says":
-
-        break;
+function main() {
+    switch (commands) {
+        // case "concert-this":
+        //     concertThis();
+        //     break;
+        case "spotify-this-song":
+            console.log("I'm in Spotify this song");
+            spotifySong();
+            break;
+        // case "movie-this":
+        //     break;
+        // case "do-what-it-says":
+        //     break;
+    }
 }
+function spotifySong() {
+    console.log("i'm in spotify song");
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        console.log(data);
 
-var getArtistNames = function(artist) {
-    return artist.name;
-}
-var getMeSpotify = function(songName) {
-
-
-function spotifySong(){
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
         if (err) {
             console.log('Error occurred: ' + err);
             return;
+
         }
-       
-      var songs = data.tracks.items;
-      for(var i=0; i<songs.length; i++) {
-          console.log(i);
-          console.log('artist(s): ' + songs[i].artists.map(
-              getArtistNames));
-              console.log('song name: ' + songs[i].name);
-              console.log('preview song: ' + songs[i].preview_url);
-              console.log('album: ' + songs[i].album.name);
-              console.log('-------------------------------------');
-          
-      } 
-      });
+        console.log(data.tracks.items[0]);
+    })
 }
-}
-var pick = function(caseData, functionData) {
-    switch(caseData) {
-        case 'spotify-this-song':
-            getMeSpotify(functionData);
-            break;
+
+
+var getArtistNames = function (artist) {
+        return artist.name;
+    }
+    var getMeSpotify = function (songName) {
+
+
+        function spotifySong() {
+            spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+                if (err) {
+                    console.log('Error occurred: ' + err);
+                    return;
+                }
+
+                var songs = data.tracks.items;
+                for (var i = 0; i < songs.length; i++) {
+                    console.log(i);
+                    console.log('artist(s): ' + songs[i].artists.map(
+                        getArtistNames));
+                    console.log('song name: ' + songs[i].name);
+                    console.log('preview song: ' + songs[i].preview_url);
+                    console.log('album: ' + songs[i].album.name);
+                    console.log('-------------------------------------');
+
+                }
+            });
+        }
+    }
+    var pick = function (caseData, functionData) {
+        switch (caseData) {
+            case 'spotify-this-song':
+                getMeSpotify(functionData);
+                break;
             default:
                 console.log('Liri does not know that');
+        }
     }
-}
