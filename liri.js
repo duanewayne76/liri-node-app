c22ef3abb90583c409badbc394013063535bd6d3
Duane Stafford <duanestafford@Duanes-MBP.richmond.edu>
2019-08-01 18:17:57 -0400

    fixed spotify return

--- liri.js
+++ liri.js
@@ -6,73 +6,126 @@ var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
-var commands = process.argv[2];
-var value = process.argv[3];
-main();
+// Write code here to parse command line arguments and store them into variables
+// Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
+let search = process.argv[2];
+let input = process.argv.slice(3).join(" ");
 
-function main() {
-    switch (commands) {
-        // case "concert-this":
-        //     concertThis();
-        //     break;
-        case "spotify-this-song":
-            console.log("I'm in Spotify this song");
-            spotifySong();
-            break;
-        // case "movie-this":
-        //     break;
-        // case "do-what-it-says":
-        //     break;
-    }
-}
-function spotifySong() {
-    console.log("i'm in spotify song");
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-        console.log(data);
 
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
+switch (search) {
+    case "actor":
+        searchActor();
+        break;
 
-        }
-        console.log(data.tracks.items[0]);
-    })
+    case "show":
+        searchShow();
+        break;
 }
 
+function searchActor() {
+    console.log(`${input} actor`);
+    var axios = require("axios");
+    axios
+    .get(`http://api.tvmaze.com/search/people?q=${input}`)
+        .then(function (response) {
 
-var getArtistNames = function (artist) {
-        return artist.name;
-    }
-    var getMeSpotify = function (songName) {
+            console.log(`Name: ${response.data[0].person.name}`);
+            console.log(`Birthday: ${response.data[0].person.birthday}`);
+            console.log(`Gender: ${response.data[0].person.gender}`);
+            console.log(`Country: ${response.data[0].person.country}`);
+            console.log(`URL: ${response.data[0].person.url}`);
+           
+})
+}
+
+function searchShow() {
+    console.log(`${input} show`);
 
 
-        function spotifySong() {
-            spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-                if (err) {
-                    console.log('Error occurred: ' + err);
-                    return;
-                }
+    var axios = require("axios");
+    axios
+    .get(`http://api.tvmaze.com/singlesearch/shows?q=${input}`)
+        .then(function (response) {
 
-                var songs = data.tracks.items;
-                for (var i = 0; i < songs.length; i++) {
-                    console.log(i);
-                    console.log('artist(s): ' + songs[i].artists.map(
-                        getArtistNames));
-                    console.log('song name: ' + songs[i].name);
-                    console.log('preview song: ' + songs[i].preview_url);
-                    console.log('album: ' + songs[i].album.name);
-                    console.log('-------------------------------------');
+            console.log(response.data);
+        })
 
-                }
-            });
-        }
-    }
-    var pick = function (caseData, functionData) {
-        switch (caseData) {
-            case 'spotify-this-song':
-                getMeSpotify(functionData);
-                break;
-            default:
-                console.log('Liri does not know that');
-        }
-    }
+}
+
+
+
+
+
+
+
+
+// var commands = process.argv[2];
+// var value = process.argv[3];
+// main();
+
+// function main() {
+//     switch (commands) {
+//         // case "concert-this":
+//         //     concertThis();
+//         //     break;
+//         case "spotify-this-song":
+//             console.log("I'm in Spotify this song");
+//             spotifySong();
+//             break;
+//         // case "movie-this":
+//         //     break;
+//         // case "do-what-it-says":
+//         //     break;
+//     }
+// }
+// function spotifySong() {
+//     console.log("i'm in spotify song");
+//     spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+//         console.log(data);
+
+//         if (err) {
+//             console.log('Error occurred: ' + err);
+//             return;
+
+//         }
+//         console.log(data.tracks.items[0]);
+//     })
+// }
+
+
+// var getArtistNames = function (artist) {
+//         return artist.name;
+//     }
+//     var getMeSpotify = function (songName) {
+
+
+//         function spotifySong() {
+//             spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+//                 if (err) {
+//                     console.log('Error occurred: ' + err);
+//                     return;
+//                 }
+
+//                 var songs = data.tracks.items;
+//                 for (var i = 0; i < songs.length; i++) {
+//                     console.log(i);
+//                     console.log('artist(s): ' + songs[i].artists.map(
+//                         getArtistNames));
+//                     console.log('song name: ' + songs[i].name);
+//                     console.log('preview song: ' + songs[i].preview_url);
+//                     console.log('album: ' + songs[i].album.name);
+//                     console.log('-------------------------------------');
+
+//                 }
+//             });
+//         }
+//     }
+//     var pick = function (caseData, functionData) {
+//         switch (caseData) {
+//             case 'spotify-this-song':
+//                 getMeSpotify(functionData);
+//                 break;
+//             default:
+//                 console.log('Liri does not know that');
+//         }
+//     }
