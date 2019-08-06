eb661f5bab25eaa3f8edc33d531e7116b27c4342
Duane Stafford <duanestafford@Duanes-MBP.richmond.edu>
2019-08-06 17:51:38 -0400

    fixed spotify

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+var fs = require("fs");
+var moment = require("moment");
 
 var axios = require('axios');
 var keys = require("./keys.js");
@@ -13,119 +15,158 @@ let input = process.argv.slice(3).join(" ");
 
 
 switch (search) {
-    case "actor":
-        searchActor();
+    case "concert-this":
+        searchConcert();
         break;
 
-    case "show":
-        searchShow();
+    case "spotify-this-song":
+        searchSong();
         break;
+
+    case "movie-this":
+        searchMovie();
+        break;
+
+    case "do-what-it-says":
+        searchDo();
+        break;
+
+
 }
 
-function searchActor() {
-    console.log(`${input} actor`);
+function searchConcert() {
+    console.log(`${input} concert`);
     var axios = require("axios");
     axios
-    .get(`http://api.tvmaze.com/search/people?q=${input}`)
+        .get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`)
         .then(function (response) {
+            for (let i = 0; i < response.data.length; i++) {
 
-            console.log(`Name: ${response.data[0].person.name}`);
-            console.log(`Birthday: ${response.data[0].person.birthday}`);
-            console.log(`Gender: ${response.data[0].person.gender}`);
-            console.log(`Country: ${response.data[0].person.country}`);
-            console.log(`URL: ${response.data[0].person.url}`);
-           
-})
-}
+                console.log(`Venue: ${response.data[i].venue.name}`);
+                console.log(`Location: ${response.data[i].venue.city}`);
+                console.log(`Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
+                console.log("-----------------------------------------------");
 
-function searchShow() {
-    console.log(`${input} show`);
+            };
 
-
-    var axios = require("axios");
-    axios
-    .get(`http://api.tvmaze.com/singlesearch/shows?q=${input}`)
-        .then(function (response) {
-
-            console.log(response.data);
         })
+}
+
+
+
+
+function runSpotifyQuery(song) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        // console.log(JSON.stringify(data.tracks.items));
+        console.log("Artist(s):", data.tracks.items[0].album.artists[0].name);
+        console.log("Song Name:", data.tracks.items[0].name);
+        console.log("Album Name:", data.tracks.items[0].album.name);
+        console.log("Preview URL:", data.tracks.items[0].preview_url);
+
+    });
+}
+
+
+function searchSong() {
+    console.log(`${input} song`);
+    console.log("type of input", input.length);
+    if (input.length == 0) {
+        console.log("That didn't work");
+        input = "Ace of Base";
+        // change here
+        runSpotifyQuery(input);
+    } else {
+        runSpotifyQuery(input);
+        //Needs to default to Ace of Base "The Sign" if nothing is entered
+
+
+    }
+}
+
+
+
+
+function searchMovie() {
+    // console.log(`${input} movie`);
+    if (input.length == 0) {
+        console.log("That didn't work");
+        input = "Mr.Nobody";
+        axios
+        .get(`http://www.omdbapi.com/?t=${input}&apikey=19cce332`)
+        .then(function (response) {
+            // console.log(response);
+            console.log(`Title: ${response.data.Title}`);
+            console.log(`Year: ${response.data.Year}`);
+            console.log(`IMDB Rating: ${response.data.imdbRating}`);
+            console.log(`Rotten Tomatoes: ${response.data.Ratings[1].Value}`);
+            console.log(`Country: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Plot: ${response.data.Plot}`);
+            console.log(`Actors: ${response.data.Actors}`);
+
+        })
+    }
+    else {
+        
+        axios
+            .get(`http://www.omdbapi.com/?t=${input}&apikey=19cce332`)
+            .then(function (response) {
+                // console.log(response);
+                console.log(`Title: ${response.data.Title}`);
+                console.log(`Year: ${response.data.Year}`);
+                console.log(`IMDB Rating: ${response.data.imdbRating}`);
+                console.log(`Rotten Tomatoes: ${response.data.Ratings[1].Value}`);
+                console.log(`Country: ${response.data.Country}`);
+                console.log(`Language: ${response.data.Language}`);
+                console.log(`Plot: ${response.data.Plot}`);
+                console.log(`Actors: ${response.data.Actors}`);
+
+            })
+    }
+    //Needs to default to Mr. Nobody if nothing is entered
 
 }
 
 
 
 
+function searchDo() {
+
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+        console.log(data);
+        data = data.split(",");
+        var action = data[0];
+        var query = data[1];
+        switch (action) {
+            case "concert-this":
+                searchConcert(query);
+                break;
+
+            case "spotify-this-song":
+                searchSong(query);
+                break;
+
+            case "movie-this":
+                searchMovie(query);
+                break;
+
+            case "do-what-it-says":
+                searchDo(query);
+                break;
+
+
+
+        }
+    });
 
 
 
 
-// var commands = process.argv[2];
-// var value = process.argv[3];
-// main();
-
-// function main() {
-//     switch (commands) {
-//         // case "concert-this":
-//         //     concertThis();
-//         //     break;
-//         case "spotify-this-song":
-//             console.log("I'm in Spotify this song");
-//             spotifySong();
-//             break;
-//         // case "movie-this":
-//         //     break;
-//         // case "do-what-it-says":
-//         //     break;
-//     }
-// }
-// function spotifySong() {
-//     console.log("i'm in spotify song");
-//     spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-//         console.log(data);
-
-//         if (err) {
-//             console.log('Error occurred: ' + err);
-//             return;
-
-//         }
-//         console.log(data.tracks.items[0]);
-//     })
-// }
+}
 
 
-// var getArtistNames = function (artist) {
-//         return artist.name;
-//     }
-//     var getMeSpotify = function (songName) {
 
-
-//         function spotifySong() {
-//             spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-//                 if (err) {
-//                     console.log('Error occurred: ' + err);
-//                     return;
-//                 }
-
-//                 var songs = data.tracks.items;
-//                 for (var i = 0; i < songs.length; i++) {
-//                     console.log(i);
-//                     console.log('artist(s): ' + songs[i].artists.map(
-//                         getArtistNames));
-//                     console.log('song name: ' + songs[i].name);
-//                     console.log('preview song: ' + songs[i].preview_url);
-//                     console.log('album: ' + songs[i].album.name);
-//                     console.log('-------------------------------------');
-
-//                 }
-//             });
-//         }
-//     }
-//     var pick = function (caseData, functionData) {
-//         switch (caseData) {
-//             case 'spotify-this-song':
-//                 getMeSpotify(functionData);
-//                 break;
-//             default:
-//                 console.log('Liri does not know that');
-//         }
-//     }
